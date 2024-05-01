51311d1b2ad7c02d525f0b8a8f52d498fb0bf550
Oleksii Kryvtsov <nightmarelie@gmail.com>
2024-05-01 10:01:11 -0600

    chore: update rust file

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
 fn main() {
-    let a: Vec<i32> = vec![];
-    let mut b = a;
+    let mut a: Vec<i32> = vec![];
+    let b = &mut a;
 
     b.push(1);
 
     println!("{:?}", b);
+    println!("{:?}", a);
 }
