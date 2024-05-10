06a4c8e0e8cc811605daa846c04af498930b2344
Oleksii Kryvtsov <nightmarelie@gmail.com>
2024-05-10 10:01:11 -0600

    chore: rust custom oprion with methods

--- src/main.rs
+++ src/main.rs
@@ -5,10 +5,17 @@ enum Option2<T> {
 }
 
 impl <T> Option2<T> {
-    fn unwrap(self) -> T {
+    fn unwrap(&self) -> &T {
         match self {
             Option2::None => panic!("called `Option::unwrap()` on a `None` value"),
             Option2::Some(value) => value,
+        }
+    }
+
+    fn is_some(&self) -> bool {
+        match self {
+            Option2::None => false,
+            Option2::Some(_) => true,
         }
     }
 }
@@ -16,7 +23,9 @@ impl <T> Option2<T> {
 fn main() {
     let x = Option2::Some(10);
     println!("{:?}", x.unwrap());
+    println!("{:?}", x.is_some());
 
     let y: Option2<i32> = Option2::None;
     println!("{:?}", y.unwrap());
+    println!("{:?}", y.is_some());
 }
