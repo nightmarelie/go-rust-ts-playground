61e0f3d2d22c9139c7aecac472db90aff9bd6675
Oleksii Kryvtsov <nightmarelie@gmail.com>
2024-05-13 10:01:11 -0600

    chore(go): create custom error type within struct

--- cmd/main.go
+++ cmd/main.go
@@ -6,8 +6,20 @@ func returnsError(value int) error {
 	return fmt.Errorf("error: %d", value)
 }
 
+type Foo struct{}
+
+func (f Foo) returnsError(value int) error {
+	return fmt.Errorf("error: %d", value)
+}
+
 func main() {
 	err := returnsError(1)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	f := Foo{}
+	err = f.returnsError(2)
 	if err != nil {
 		fmt.Println(err)
 	}
